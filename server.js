25cd5ebec2f89cf579b681fe6b24915f78369a67
pfazekas1 <91687004+pfazekas1@users.noreply.github.com>
2023-01-18 20:52:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,30 +4,4 @@ const app = express();
 
 
 const server = require('http').createServer(app);
-
-
-const io = require('socket.io')(server, {
-    cors: { origin: "*"}
-});
-
-
-io.on('connection', (socket) => {
-    console.log('connection');
-
-    socket.on('sendChatToServer', (message) => {
-        console.log(message);
-
-        // io.sockets.emit('sendChatToClient', message);
-        socket.broadcast.emit('sendChatToClient', message);
-    });
-
-
-    socket.on('disconnect', (socket) => {
-        console.log('Disconnect');
-    });
-});
-
-server.listen(3000, () => {
-    console.log('Server is running');
-});
-//https://www.youtube.com/watch?v=MNf0piqRdHg
+const port = process.env.port || 3000;
